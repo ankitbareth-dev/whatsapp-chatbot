7a869445332686ab2e28f198d25e547c2df1bd74
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-21 12:14:04 +0530

    greeting component added

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import TopBar from "@/components/TopBar";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="w-full min-h-screen bg-[#0D1720] text-white p-4 md:p-6">
+      <TopBar />
+    </div>
+  );
 };
 
 export default Dashboard;
